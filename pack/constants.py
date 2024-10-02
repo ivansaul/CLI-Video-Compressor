1b945affc752a839599be0c280dfa564c25a39e9
ivansaul <saul_2503@hotmail.com>
2024-10-02 01:11:53 -0500

    typo: replace 'override' with 'overwrite'

--- pack/constants.py
+++ pack/constants.py
@@ -3,7 +3,7 @@ class Constants:
     INPUT_HELP_TEXT = "The input file video to compress."
     OUTPUT_HELP_TEXT = "The output file to save the compressed video."
     DEBUG_HELP_TEXT = "Enable debugging."
-    OVERRIDE_HELP_TEXT = "Override existing output file."
+    OVERWRITE_HELP_TEXT = "Overwrite existing output file."
     UTILS_PANEL_TEXT = "Customization and Utils"
     ERROR_MESSAGE = "An error occurred while compressing the video 💥."
     FFMPEG_NOT_INSTALLED = "[bold red]ffmpeg is not installed! 💥[/bold red]"
