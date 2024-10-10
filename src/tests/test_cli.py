e6035372aca0a9ba299f7cd2eb757cd6d4e18e8f
ivansaul <saul_2503@hotmail.com>
2024-10-09 19:01:10 -0500

    dasda

--- src/tests/test_cli.py
+++ src/tests/test_cli.py
@@ -28,14 +28,14 @@ def test_single_file_compression_with_default_options():
 
 @pytest.mark.parametrize("codec", ["h264", "libx265"])
 def test_single_file_compression(codec):
-    result = runner.invoke(app, [INPUT, "--codec", codec])
+    result = runner.invoke(app, [INPUT, "--codec", codec], color=False)
     assert result.exit_code == 0, result.stdout
     assert PROGRESS_COMPLETION_OUTPUT in result.stdout
 
 
 @pytest.mark.parametrize("codec", ["libx267", "123"])
 def test_single_file_compression_with_invalid_codec(codec):
-    result = runner.invoke(app, ["input.mp4", "--codec", codec])
+    result = runner.invoke(app, ["input.mp4", "--codec", codec], color=False)
     expected_message = f"Invalid value for '--codec' / '-c': '{codec}'"
     assert result.exit_code == 2, result.stdout
     assert expected_message in result.stdout, result.stdout
