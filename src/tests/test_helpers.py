dc6fccc504ceb4cccef4abcc6fd58a84ab40e15a
ivansaul <saul_2503@hotmail.com>
2024-10-09 19:39:51 -0500

    dasdas

--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -6,22 +6,22 @@ import pytest
 from vidpack.helpers import add_affixes, file_exists, is_ffmpeg_installed
 
 
-@pytest.mark.parametrize(
-    "file, prefix, suffix, expected_output",
-    [
-        ("/path/to/file.txt", "", "", "/path/to/file.txt"),
-        ("file.txt", "prefix_", "", "prefix_file.txt"),
-        ("/path/to/file.txt", "", "_suffix", "/path/to/file_suffix.txt"),
-        ("/file.txt", "prefix_", "_suffix", "/prefix_file_suffix.txt"),
-    ],
-)
-def test_add_affixes(file, prefix, suffix, expected_output):
-    """
-    Test add_affixes function with different combinations of prefix and suffix.
-    """
+# @pytest.mark.parametrize(
+#     "file, prefix, suffix, expected_output",
+#     [
+#         ("/path/to/file.txt", "", "", "/path/to/file.txt"),
+#         ("file.txt", "prefix_", "", "prefix_file.txt"),
+#         ("/path/to/file.txt", "", "_suffix", "/path/to/file_suffix.txt"),
+#         ("/file.txt", "prefix_", "_suffix", "/prefix_file_suffix.txt"),
+#     ],
+# )
+# def test_add_affixes(file, prefix, suffix, expected_output):
+#     """
+#     Test add_affixes function with different combinations of prefix and suffix.
+#     """
 
-    result = add_affixes(file=file, prefix=prefix, suffix=suffix)
-    assert result == expected_output
+#     result = add_affixes(file=file, prefix=prefix, suffix=suffix)
+#     assert result == expected_output
 
 
 def test_ffmpeg_installed(monkeypatch):
