da5640e26d77327a0071309e9c6f4ca255f63e77
ivansaul <saul_2503@hotmail.com>
2024-10-10 03:05:10 -0500

    feat: add basic unit tests
    Adds basic unit tests for the CLI and helper functions.
    Includes:
    - Test cases for compression with various codecs, quality settings, overwrite options, and output paths.
    - Tests for checking if FFmpeg is installed and file existence.

--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -1,4 +1,5 @@
 import subprocess
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
@@ -9,19 +10,18 @@ from vidpack.helpers import add_affixes, file_exists, is_ffmpeg_installed
 @pytest.mark.parametrize(
     "file, prefix, suffix, expected_output",
     [
-        ("/path/to/file.txt", "", "", "/path/to/file.txt"),
-        ("file.txt", "prefix_", "", "prefix_file.txt"),
-        ("/path/to/file.txt", "", "_suffix", "/path/to/file_suffix.txt"),
-        ("/file.txt", "prefix_", "_suffix", "/prefix_file_suffix.txt"),
+        (Path("/path/to/file.txt"), "", "", Path("/path/to/file.txt")),
+        (Path("file.txt"), "prefix_", "", Path("prefix_file.txt")),
+        (Path("/path/to/file.txt"), "", "_suffix", Path("/path/to/file_suffix.txt")),
+        (Path("/file.txt"), "prefix_", "_suffix", Path("/prefix_file_suffix.txt")),
     ],
 )
 def test_add_affixes(file, prefix, suffix, expected_output):
     """
     Test add_affixes function with different combinations of prefix and suffix.
     """
-
-    result = add_affixes(file=file, prefix=prefix, suffix=suffix)
-    assert result == expected_output
+    result = add_affixes(file=str(file), prefix=prefix, suffix=suffix)
+    assert result == str(expected_output)
 
 
 def test_ffmpeg_installed(monkeypatch):
