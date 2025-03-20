cd240133d26ffe9be0f61145e931434d9eac91ec
ivansaul <saul_2503@hotmail.com>
2025-03-19 20:39:09 -0500

    refactor: improve test cleanup logic

--- src/tests/test_helpers.py
+++ src/tests/test_helpers.py
@@ -1,14 +1,12 @@
-import subprocess
 from pathlib import Path
-from unittest.mock import patch
 
 import pytest
 
-from vidpack.helpers import add_affixes, is_ffmpeg_installed, path_exists
+from vidpack.helpers import add_affixes
 
 
 @pytest.mark.parametrize(
-    "file, prefix, suffix, expected_output",
+    "target, prefix, suffix, expected_output",
     [
         (Path("/path/to/file.txt"), "", "", Path("/path/to/file.txt")),
         (Path("file.txt"), "prefix_", "", Path("prefix_file.txt")),
@@ -16,50 +14,9 @@ from vidpack.helpers import add_affixes, is_ffmpeg_installed, path_exists
         (Path("/file.txt"), "prefix_", "_suffix", Path("/prefix_file_suffix.txt")),
     ],
 )
-def test_add_affixes(file, prefix, suffix, expected_output):
+def test_add_affixes(target, prefix, suffix, expected_output):
     """
     Test add_affixes function with different combinations of prefix and suffix.
     """
-    result = add_affixes(file=str(file), prefix=prefix, suffix=suffix)
-    assert result == str(expected_output)
-
-
-def test_ffmpeg_installed(monkeypatch):
-    """
-    Test that ffmpeg is installed by mocking subprocess.run.
-    """
-
-    # Simulate that ffmpeg is installed
-    def mock_run(*args, **kwargs):
-        return True
-
-    monkeypatch.setattr(subprocess, "run", mock_run)
-    assert is_ffmpeg_installed() is True
-
-
-def test_ffmpeg_not_installed(monkeypatch):
-    """
-    Test that ffmpeg is not installed by mocking subprocess.run to raise FileNotFoundError.
-    """
-
-    # Simulate that ffmpeg is not installed
-    def mock_run(*args, **kwargs):
-        raise FileNotFoundError
-
-    monkeypatch.setattr(subprocess, "run", mock_run)
-
-    assert is_ffmpeg_installed() is False
-
-
-@patch("vidpack.helpers.Path.exists")
-def test_path_exists(mock_exists):
-    """
-    Test path_exists function with different scenarios.
-    """
-    # Simulate that the file exists
-    mock_exists.return_value = True
-    assert path_exists("/path/to/existing_file.txt") is True
-
-    # Simulate that the file does not exist
-    mock_exists.return_value = False
-    assert path_exists("/path/to/non_existent_file.txt") is False
+    result = add_affixes(target=target, prefix=prefix, suffix=suffix)
+    assert result == expected_output
